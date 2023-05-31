83b7341e811f533ed575cb7815effd932091e787
Duel <51247353+marteiduel@users.noreply.github.com>
2023-05-31 12:54:19 -0600

    font family and most used techs icons

--- src/components/main/Tile.js
+++ src/components/main/Tile.js
@@ -1,35 +1,40 @@
 import { Link } from "react-router-dom";
+import TechnologyIcons from "../full-post/TechnologyIcons";
 
 const Tile = ({ post }) => {
+  const mostUsedTechs = post.technologies.split(",").slice(0, 4).join(",");
   return (
-    <div className="w-80 border border-black justify-center m-4 rounded mb-4 h-tileHeight">
+    <Link
+      to={`/post/${post.id}`}
+      className="w-80 border-2 hover:border-gray-400 justify-center m-4 rounded-2xl mb-4 h-tileHeight shadow"
+    >
       <div>
         {post.demoImage ? (
           <img
-            className="rounded"
+            className="rounded-2xl p-2 "
             alt={`${post.title}`}
             src={`assets/demo/${post.demoImage}`}
           />
         ) : (
           <img
-            className="rounded"
+            className="rounded-2xl p-2 "
             src={`assets/pictures/default-image.jpg`}
             alt={`${post.title}`}
           />
         )}
       </div>
       <div className="body flex items-center flex-col h-full">
-        <h1 className="text-lg font-semibold mt-4">{post.title}</h1>
+        <h1 className="text-2xl font-semibold mt-4">{post.title}</h1>
         <p className="text-sm mt-2">{post.date}</p>
         <p className="m-4">{post.description}</p>
-        <Link
-          to={`/post/${post.id}`}
-          className="border border-black rounded-lg w-32 h-8 m-2 flex justify-center items-center"
-        >
-          Read More...
-        </Link>
+        <div>
+          {" "}
+          {post.technologies !== null ? (
+            <TechnologyIcons technologies={mostUsedTechs} />
+          ) : null}
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
